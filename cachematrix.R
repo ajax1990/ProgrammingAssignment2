832ac9364ba1db62613f7875764ac522e5cb4ad1
ajax1990 <ajay_pillai@live.in>
2014-08-14 11:16:07 +0530

    i update the empty functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function i design get special matrix object at early time
 
 makeCacheMatrix <- function(x = matrix()) {
+## Initialize the inverse property
+    inv <- NULL
 
+    ## Matrix set 1
+    set <- function( matrix ) {
+            x <<- matrix
+            inv <<- NULL
+    }
+
+    ## Matrix get 1
+    get <- function() {
+    	## Return the matrix
+    	x
+    }
+
+    ## Set inverse matrix
+    setInverse <- function(inverse) {
+        inv <<- inverse
+    }
+
+    ## Inverse of the matrix get
+    getInverse <- function() {
+        ## Inverse property return
+        inv
+    }
+
+    ## Method list return
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+
+	
 }
 
 
-## Write a short comment describing this function
+## Inverse of the special matrix returned by makecachematrix is calculate , not exist return cache inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## inverse of 'x' is return
+    y <- x$getInverse()
+
+    ## cache is return if there 
+    if( !is.null(y) ) {
+            message("Cache data is get")
+            return(y)
+    }
+
+    ## Matrix is get from special object
+    data <- x$get()
+
+    ## Matrix multiplication get inverse
+    y <- solve(data) %*% data
+
+    ## Object inverse set
+    x$setInverse(y)
+
+    ## Return the matrix
+    y
+   
 }
